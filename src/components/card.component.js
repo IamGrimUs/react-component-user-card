c5373aa39e5b453156b5e469b250b1876377fc83
r-dbl-l <iamgrimus@gmail.com>
2020-12-21 23:56:36 -0800

    user card refactor

--- src/components/card.component.js
+++ src/components/card.component.js
@@ -1,20 +1,10 @@
 import * as React from 'react';
 import './card.styles.css'
 
-export const Card = ({user, sections}) => {
+export const Card = ({children}) => {
   return (
     <div className="card-frame center bg-white">
-      <div className="card-background-pattern" style={{ backgroundImage: `url(${user.background})` }}></div>
-      <figure className="avatar center"><img src={user.avatar} alt={user.firstName} /></figure>
-      <div class="card-details">
-        <p>{`${user.firstName} ${user.lastName}`} <span class="">{ user.age}</span></p>
-        <p>{user.country}</p>
-      </div>
-      <div className="flex-box three-col">
-        <div><p>{user.followers}K</p><p>{sections[0]}</p></div>
-        <div><p>{user.likes}K</p><p>{sections[1]}</p></div>
-        <div><p>{user.photos}K</p><p>{sections[2]}</p></div>
-      </div>
+      {children}
     </div>
   )
 }
--- src/userCard/userCard.component.js
+++ src/userCard/userCard.component.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import faker from 'faker'
 import {Card} from '../components/card.component.js'
+import './userCard.styles.css'
 
 const { useState } = React;
 
@@ -10,17 +11,33 @@ const currentUser = {
   firstName: faker.name.firstName(),
   lastName: faker.name.lastName(),
   age: faker.random.number(),
-  followers: faker.random.number(),
-  likes: faker.random.number(),
-  photos: faker.random.number(),
-  country: faker.address.country()
+  country: faker.address.country(),
+  userStats: [{ followers: faker.random.number() }, {likes: faker.random.number() }, {photos: faker.random.number() }]
 }
 
-const sectionTitles = ['Followers', 'Likes', 'Photos'];
+const renderUserStats = (user) => {
+  const { userStats } = user;
+  return userStats.map((stat, index) => {
+    const key = Object.keys(stat)
+    return <div key={`panel-${index}`}><p>{stat[key]}K</p><p>{Object.keys(stat)}</p></div>
+  })
+}
 
 export const UserCard = () => {
   const [user, setUser] = useState(currentUser)
   return (
-    <Card user={user} sections={sectionTitles} />
+    <div className="extra-mark-up">
+      <Card>
+        <div className="card-background-pattern" style={{ backgroundImage: `url(${user.background})` }}></div>
+        <figure className="avatar center"><img src={user.avatar} alt={user.firstName} /></figure>
+        <div className="card-details">
+          <p>{`${user.firstName} ${user.lastName}`} <span>{ user.age}</span></p>
+          <p>{user.country}</p>
+        </div>
+        <div className="flex-box user-stats">
+          {renderUserStats(user)}
+        </div>
+      </Card>
+    </div>
   )
 }
